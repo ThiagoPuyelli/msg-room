89a28561be300fd6bda05946f707713f5aaad6cc
ThiagoPuyelli <thiagopuyelli@gmail.com>
2021-06-22 13:18:36 -0300

    Arreglo de auth en socket

--- backend/src/sockets.ts
+++ backend/src/sockets.ts
@@ -1,7 +1,7 @@
 import { Application } from 'express'
 import { Server } from 'http'
-// import jwt from 'jsonwebtoken'
-// import User from './models/User'
+import jwt from 'jsonwebtoken'
+import User from './models/User'
 import session from 'express-session'
 import passport from 'passport'
 
@@ -17,29 +17,27 @@ export const setSockets = (app: Application) => {
     resave: false,
     saveUninitialized: false
   })))
-  io.use(wrap(passport.initialize()))
-  io.use(wrap(passport.session()))
-  io.use(wrap(passport.authenticate('token')))
+    .use(wrap(passport.initialize()))
+    .use(wrap(passport.session()))
+    .use(async (socket, next) => {
+      const token = socket.handshake.auth.token
 
-  /* io.use(async (socket, next) => {
-    const token = socket.handshake.auth.token
-    if (!token || token === '') {
-      return next(new Error('Unauthorized'))
-    }
-    const payload = jwt.verify(token, process.env.JWT_PASSWORD)
+      if (!token || token === '') {
+        return next(new Error('Unauthorized'))
+      }
 
-    if (!payload) {
-      return next(new Error('The token is invalid'))
-    }
+      const payload = jwt.verify(token, process.env.JWT_PASSWORD)
+      if (!payload) {
+        return next(new Error('The token is invalid'))
+      }
 
-    const user = await User.findById(payload.userID)
+      const user = await User.findById(payload.userID)
+      if (!user) {
+        return next(new Error('The user is doesn\'t exist'))
+      }
 
-    if (!user) {
-      return next(new Error('The user is doesn\'t exist'))
-    }
-
-    return next(null, user)
-  }) */
+      return next(null, user)
+    })
 
   io.on('connection', (socket) => {
     socket.emit('saludo', 'hola pibe')
