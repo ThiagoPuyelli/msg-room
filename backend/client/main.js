70d41c27fb07fde85eaa4383b0fc9fcc0169ea28
ThiagoPuyelli <thiagopuyelli@gmail.com>
2021-06-23 21:51:37 -0300

    Join rrom

--- backend/client/main.js
+++ backend/client/main.js
@@ -6,6 +6,24 @@ const socket = io({
   }
 }).connect('http://localhost:6200', { forceNew: true })
 
-socket.on('saludo', (data) => {
-  console.log(data)
-})
+socket.on('receive-messages', (messages) => console.log(messages))
+
+// eslint-disable-next-line no-unused-vars
+const submitJoin = () => {
+  window.event.preventDefault()
+  const idChat = document.querySelector('#chat').value
+
+  socket.emit('join-friend', idChat, pepe => {
+    console.log(pepe)
+  })
+}
+
+// eslint-disable-next-line no-unused-vars
+const submitMessage = () => {
+  window.event.preventDefault()
+
+  const message = document.querySelector('#message').value
+  const idChat = document.querySelector('#chat').value
+
+  socket.emit('send-message', message, idChat)
+}
--- backend/src/sockets.ts
+++ backend/src/sockets.ts
@@ -2,24 +2,14 @@ import { Application } from 'express'
 import { Server } from 'http'
 import jwt from 'jsonwebtoken'
 import User from './models/User'
-import session from 'express-session'
-import passport from 'passport'
 
 export const setSockets = (app: Application) => {
   const server = new Server(app)
 
   const io = require('socket.io')(server)
 
-  const wrap = middleware => (socket, next) => middleware(socket.request, {}, next)
-
-  io.use(wrap(session({
-    secret: process.env.SESSION_PASS,
-    resave: false,
-    saveUninitialized: false
-  })))
-    .use(wrap(passport.initialize()))
-    .use(wrap(passport.session()))
-    .use(async (socket, next) => {
+  io.use(async (socket, next) => {
+    try {
       const token = socket.handshake.auth.token
 
       if (!token || token === '') {
@@ -36,12 +26,30 @@ export const setSockets = (app: Application) => {
         return next(new Error('The user is doesn\'t exist'))
       }
 
+      socket.data.user = user
       return next(null, user)
-    })
-
-  io.on('connection', (socket) => {
-    socket.emit('saludo', 'hola pibe')
+    } catch (err) {
+      next(new Error(err.message || 'Server error'))
+    }
   })
+    .on('connection', (socket) => {
+      socket.on('join-friend', async (chat, cb) => {
+        try {
+          const friend = socket.data.user.friends.find(friend => String(friend.user) === String(chat))
+          if (friend) {
+            socket.join(friend.user)
+            return cb(`Joined ${chat}`)
+          }
+        } catch (err) {
+          console.log(err)
+        }
+      })
+
+      socket.on('send-message', (message, room) => {
+        console.log(socket.rooms)
+        socket.to(room).emit('receive-messages', message)
+      })
+    })
 
   return server
 }
